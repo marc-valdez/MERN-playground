4c2464e5e872a094ac90cd6f8a1abdd39aa84938
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2025-05-19 09:02:33 +0800

    Complete all API endpoints
    Adjusted how errors are returned.
    Error 500 should be the generic error.
    Error 404 occurs when an object doesn't exist in the db.
    Error 400 when it's an invalid ID format.

--- backend/controllers/WorkoutController.js
+++ backend/controllers/WorkoutController.js
@@ -12,17 +12,17 @@ const getWorkout = async (req, res) => {
   const { id } = req.params;
   try {
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      throw new Error('No such workout');
+      return res.status(400).json({ error: 'Invalid ID format' });
     }
 
     const workout = await Workout.findById(id);
     if (!workout) {
-      throw new Error('No such workout');
+      return res.status(404).json({ error: 'No such workout' });
     }
 
     res.status(200).json(workout);
   } catch (error) {
-    res.status(404).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -34,16 +34,56 @@ const createWorkout = async (req, res) => {
     const workout = await Workout.create({ title, reps, load });
     res.status(200).json(workout);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
 // DELETE a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const workout = await Workout.findOneAndDelete({ _id: id });
+    if (!workout) {
+      return res.status(404).json({ error: 'No such workout' });
+    }
+
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 // UPDATE a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const workout = await Workout.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+    );
+    if (!workout) {
+      return res.status(404).json({ error: 'No such workout' });
+    }
+
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
   getAllWorkouts,
   getWorkout,
   createWorkout,
+  deleteWorkout,
+  updateWorkout,
 };
